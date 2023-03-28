6c31d8ff444a5faccc7be54576fad1e849d84d90
Trace R Linton <117109227+Trace1983@users.noreply.github.com>
2023-03-28 12:27:07 -0400

    updates

---
+++ Develop/script.js
@@ -0,0 +1,39 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+// Generate Password
+function generatePassword() {
+  // Password length variable
+  var pwLength = prompt("How many characters would you like your password to contain?");
+
+  // Rejects password length if not chosen between 8 and 128
+  if (pwLength < 8 || pwLength > 128) {
+    alert("Password length must be between 8 and 128 characters. Please try again.");
+  }
+
+  // Variables for user password requirements
+  var special = confirm("Click OK to include special characters.");
+  var numeric = confirm("Click OK to include numeric characters.");
+  var lowerCase = confirm("Click OK to include lowercase characters.");
+  var upperCase = confirm("Click OK to include uppercase characters.");
+
+  // Rejects password if none of the requirements are chosen
+  if (special === false && numeric === false && lowerCase === false && upperCase === false) {
+    alert("You must choose at least one character type. Please try again.");
+  }
+
+  // Variables for all possible characters
+  var specialChars = "~!@#$%^&*()_+-={} [] 
